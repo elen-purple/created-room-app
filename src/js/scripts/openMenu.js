fab7bf08c310af5f536c4178ef42f00112e164ba
elen-purple <anna.havrylova.2011@gmail.com>
2025-07-28 00:13:03 +0300

    try 4

--- src/js/scripts/openMenu.js
+++ src/js/scripts/openMenu.js
@@ -1,6 +1,4 @@
 export const openMenu = (x, y) => {
-  console.log("eee");
-  console.log(document.querySelector("#menu"));
   document.querySelector("#menu").classList.remove("is-hidden");
   document.querySelector("#menu").style.left = `${x}px`;
   document.querySelector("#menu").style.top = `${y}px`;
