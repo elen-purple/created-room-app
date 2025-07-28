b3a04d15f4a47da18ccaa6f6de988fac7ec0a2f1
lika-ruby <mary.havrylova.2011@gmail.com>
2025-07-28 21:30:47 +0300

    aaaaaa

--- src/js/markups/makesBgList.js
+++
@@ -1,8 +0,0 @@
-export const makesBgList = (furniture) => {
-  return furniture.map(
-    (f) => `<li class="sidebar__item">
-        <img id="img-${f.id}" class="sidebar__img" src="${f.img}" alt="Furniture" />
-      </li>`
-  )
-    .join("");
-};
